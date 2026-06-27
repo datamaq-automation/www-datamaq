fad4963dddeb12a81588b5838dcc021c9843bc83
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2026-06-27 10:43:18 -0300

    style(cookie): optimizar diseno, posicionamiento y comportamiento en moviles del banner de cookies

--- static/js/modules/CookieManager.js
+++ static/js/modules/CookieManager.js
@@ -14,12 +14,14 @@ export const initCookieManager = (bannerElement, acceptBtn, rejectBtn, loadScrip
         } else if (consent === null) {
             console.debug("[CookieManager] Mostrando banner de cookies.");
             bannerElement.classList.add('is-visible');
+            document.body.classList.add('has-consent-banner');
         }
 
         acceptBtn.addEventListener('click', () => {
             console.info("[CookieManager] Usuario aceptó cookies.");
             localStorage.setItem('userConsent', 'accepted');
             bannerElement.classList.remove('is-visible');
+            document.body.classList.remove('has-consent-banner');
             loadScripts();
         });
 
@@ -27,6 +29,7 @@ export const initCookieManager = (bannerElement, acceptBtn, rejectBtn, loadScrip
             console.info("[CookieManager] Usuario rechazó cookies.");
             localStorage.setItem('userConsent', 'rejected');
             bannerElement.classList.remove('is-visible');
+            document.body.classList.remove('has-consent-banner');
         });
     } catch (error) {
         console.error("[CookieManager] Error accediendo a localStorage:", error);
